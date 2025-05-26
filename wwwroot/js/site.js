1c3a9d5e28c987848152ccb8a94d412ccd19002f
Semih Karakaya <semihkarakaya47@gmail.com>
2025-05-27 02:00:36 +0300

    some view changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,92 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    // Navbar scroll effect
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    });
 
-// Write your JavaScript code.
+    // DataTable default settings
+    $.extend(true, $.fn.dataTable.defaults, {
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.13.4/i18n/tr.json"
+        },
+        "responsive": true,
+        "pageLength": 25,
+        "dom": "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>"
+    });
+
+    // Auto-hide alerts after 5 seconds
+    $('.alert').delay(5000).fadeOut();
+
+    // Add fade-in animation to cards
+    $('.card').addClass('fade-in');
+
+    // Smooth scrolling for anchor links
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 100
+            }, 1000);
+        }
+    });
+});
+
+// Loading button state
+function setLoadingState(button, loading = true) {
+    if (loading) {
+        $(button).prop('disabled', true)
+            .html('<span class="loading me-2"></span>Yükleniyor...');
+    } else {
+        $(button).prop('disabled', false);
+    }
+}
+
+// Success notification
+function showNotification(message, type = 'success') {
+    const iconMap = {
+        'success': 'fa-check-circle',
+        'error': 'fa-exclamation-triangle',
+        'warning': 'fa-exclamation-circle',
+        'info': 'fa-info-circle'
+    };
+
+    const alert = `
+        <div class="alert alert-${type} alert-dismissible fade show position-fixed" 
+             style="top: 100px; right: 20px; z-index: 9999; min-width: 300px;" role="alert">
+            <i class="fas ${iconMap[type]} me-2"></i>${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>
+    `;
+    $('body').append(alert);
+
+    setTimeout(() => {
+        $('.alert').fadeOut();
+    }, 5000);
+}
+
+// Confirm dialog
+function confirmAction(message, callback) {
+    if (confirm(message)) {
+        callback();
+    }
+}
+
+// Format currency
+function formatCurrency(amount) {
+    return new Intl.NumberFormat('tr-TR', {
+        style: 'currency',
+        currency: 'TRY'
+    }).format(amount);
+}
+
+// Format date
+function formatDate(date) {
+    return new Intl.DateTimeFormat('tr-TR').format(new Date(date));
+}
